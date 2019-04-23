a0a29b5abb0332820287add61bda0ff2df70f9ae
Michal Hosna <mh@michalhosna.com>
2019-04-23 18:17:30 +0200

    [WIP]

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+IFS=$'\n\t'
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPT")
+set -x
+
+cd $DIR
+
+ACME_EMAIL="hosna+test@mangoweb.cz"
+ACME_SERVER="https://acme-staging-v02.api.letsencrypt.org/directory"
+
+BASE_URL="test.docker"
+ENABLE_ADMINER=false
+DB_PASSWORD="einstore"
+
+envsubst ACME_EMAIL ACME_SERVER < config/traefik.template.toml > config/traefik.toml
+envsubst BASE_URL DB_PASSWORD ADMINER_URL < docker-compose.override.template.yaml > docker-compose.override.yaml
+
+
