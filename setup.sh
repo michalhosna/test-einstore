a7f26fe0747f8e96e5063cec97bb62194639a057
Michal Hosna <mh@michalhosna.com>
2019-04-23 18:24:24 +0200

    [WIP]

--- setup.sh
+++ setup.sh
@@ -7,14 +7,14 @@ set -x
 
 cd "$DIR"
 
-ACME_EMAIL="hosna+test@mangoweb.cz"
-ACME_SERVER="https://acme-staging-v02.api.letsencrypt.org/directory"
+export ACME_EMAIL="hosna+test@mangoweb.cz"
+export ACME_SERVER="https://acme-staging-v02.api.letsencrypt.org/directory"
 
-BASE_URL="gdqngtrjwxef4flh7u9rutmw.t.michalhosna.com"
-ENABLE_ADMINER=false
-DB_PASSWORD="einstore"
+export BASE_URL="gdqngtrjwxef4flh7u9rutmw.t.michalhosna.com"
+export ENABLE_ADMINER=false
+export DB_PASSWORD="einstore"
 
-envsubst ACME_EMAIL ACME_SERVER < config/traefik.template.toml > config/traefik.toml
-envsubst BASE_URL DB_PASSWORD ADMINER_URL < docker-compose.override.template.yaml > docker-compose.override.yaml
+envsubst '${ACME_EMAIL} ${ACME_SERVER}' < config/traefik.template.toml > config/traefik.toml
+envsubst '${BASE_URL} ${DB_PASSWORD} ${ADMINER_URL}' < docker-compose.override.template.yaml > docker-compose.override.yaml
 
 
